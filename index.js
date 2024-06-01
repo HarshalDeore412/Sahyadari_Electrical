7af8d9cf885baee6dea58ad60b894cc0f0901289
Harshal Deore <harshaldeore412@gmail.com>
2024-06-01 11:21:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+menuToggle.addEventListener("click", function () {
+  mobileMenu.classList.toggle("hidden");
+});
+
+const savetolocal = (e) => {
+  let email = e.email;
+
+  localStorage.setItem("email", email);
+
+  if (localStorage.getItem("email")) {
+    alert("your response save successfully--->");
+  }
+};
+
+
