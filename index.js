840c2b2c964d6557ccd634023d3806c93a8ace74
Harshal Deore <harshaldeore412@gmail.com>
2024-06-04 13:55:13 +0530

    added maintaince messages

--- index.js
+++ index.js
@@ -18,3 +18,18 @@ const savetolocal = (e) => {
 
 
 
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "smcharshal24@gmail.com",
+    Password: "SMC@#2024",
+    To: 'Harshaldeore412@gmail.com',
+    From: "smcharshal24@gmail.com",
+    Subject: "Sending Email using javascript",
+    Body: "Well that was easy!!",
+  })
+  .then(function (message) {
+    alert("mail sent successfully")
+  });
+}
